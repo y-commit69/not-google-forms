e9f05a137722d458419bf224427377daa7bebcf0
otabek <95689252+otabekshoyimov@users.noreply.github.com>
2024-10-24 12:20:28 +0500

    fix: responsive css

--- client/src/error-page.tsx
+++ client/src/error-page.tsx
@@ -50,13 +50,13 @@ export const ErrorPage = () => {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <div className="flex min-h-screen w-full flex-col items-center justify-center px-20">
         {error.status === 404 ? (
           <>
             <h2>{error.status}</h2>
             <p>{error.statusText}</p>
-            {error.data && <p className="mt-2">{error.data}</p>}
-            <span className="pb-20 underline decoration-red-500 decoration-wavy">
+            {/* {error.data && <p className="mt-2">{error.data}</p>} */}
+            <span className="pb-20 text-center underline decoration-red-500 decoration-wavy">
               "Sorry, we can't find the page you are looking for"
             </span>
 
