5ce8b3ee5c5ad6a2f5b6376d6ae6dcd500296d14
otabek <95689252+otabekshoyimov@users.noreply.github.com>
2024-10-25 14:02:50 +0500

    fix: cors error

--- server/server.ts
+++ server/server.ts
@@ -27,6 +27,9 @@ app.use(
       "https://not-google-forms.onrender.com",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 204,
   })
 );
 
