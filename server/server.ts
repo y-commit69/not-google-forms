85be1860191fa5d85c089207466e2e40c9430ea9
otabek <95689252+otabekshoyimov@users.noreply.github.com>
2024-11-04 17:49:49 +0500

    fix: render deploy

--- server/server.ts
+++ server/server.ts
@@ -160,7 +160,7 @@ app.get("/templates", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
